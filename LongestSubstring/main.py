1979b5980165ff5fc6526137de063d6366f34c8b
Tanay Matta <83384365+Wanderer0074348@users.noreply.github.com>
2024-07-04 11:35:12 +0400

    Delete LongestSubstring/main.py

--- LongestSubstring/main.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestCommonPrefix(self, strs: list[str]) -> str:
-        prefix = ""
-        for i in range(len(strs[0])):
-            for st in strs:
-                if i == len(st) or st[i]!=strs[0][i]:
-                    return prefix
-            prefix+=strs[0][i]
-        return prefix
